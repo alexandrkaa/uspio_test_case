f5fafcc09e8d2583a6dc79c1f444715007835dfc
Aleksandr Komarov <komar81@gmail.com>
2023-08-11 22:09:31 +0200

    Добавил fancybox 3

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,15 @@
 import '../scss/style.scss'
-import javascriptLogo from '../assets/javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import '@fancyapps/fancybox/dist/jquery.fancybox.min.css';
+// import javascriptLogo from '../assets/javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
 import $ from './jquery.js';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
+import '@fancyapps/fancybox/dist/jquery.fancybox.min.js';
 
 $(document).ready(function () {
   $('.owl-carousel').owlCarousel();
 });
 
-setupCounter(document.querySelector('#counter'))
+// setupCounter(document.querySelector('#counter'))
