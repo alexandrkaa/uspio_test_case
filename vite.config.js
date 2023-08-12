115fa81c5011e9c674874cfbe41aad1178c0475a
Aleksandr Komarov <komar81@gmail.com>
2023-08-12 12:31:19 +0200

    Добавил viteImagemin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // https://vitejs.dev/config/
 import { defineConfig } from "vite";
-// import { resolve } from "path";
+import viteImagemin from 'vite-plugin-imagemin'
+
 
 export default defineConfig({
   build: {
@@ -8,8 +9,50 @@ export default defineConfig({
     outDir: 'dist',
     cssMinify: true,
     sourcemap: true,
+    // to enable chunks comment it
+    rollupOptions: {
+      output: {
+        entryFileNames: `js/[name].js`,
+        chunkFileNames: `js/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   },
   server: {
     port: Number(process.env.CLIENT_PORT) || 3000,
   },
+  plugins: [
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+      webp: {
+        quality: 80,
+        method: 4,
+
+      }
+    }),
+  ],
 });
