f949b2ca5b0d5076a6e2ca0c21048fda19f75cbb
Aleksandr Komarov <komar81@gmail.com>
2023-08-12 23:22:18 +0200

    Внес основную разметку и экспортировал изображения

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,12 @@
 import '../scss/style.scss'
 import '@fancyapps/fancybox/dist/jquery.fancybox.min.css';
-// import javascriptLogo from '../assets/javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
 import $ from './jquery.js';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
 import '@fancyapps/fancybox/dist/jquery.fancybox.min.js';
+import { webpClass } from './utils.js';
 
+webpClass();
 $(document).ready(function () {
   $('.owl-carousel').owlCarousel();
 });
-
-// setupCounter(document.querySelector('#counter'))
